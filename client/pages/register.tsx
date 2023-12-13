a8b740e7e1201e9054b67964a0525efc8482a02b
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-13 17:23:19 +0900

    feat(ui) Register.tsx

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React, { FormEvent, useState } from "react";
+import axios from "axios";
 import InputGroup from "@/components/InputGroup";
 
 const Register = () => {
@@ -8,12 +10,31 @@ const Register = () => {
   const [password, setPassword] = useState<string>("");
   const [errors, setErrors] = useState<any>({});
 
+  let router = useRouter();
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("/auth/register", {
+        email,
+        password,
+        username,
+      });
+      console.log("response", response);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("error", error);
+      setErrors(error.response.data || {});
+    }
+  };
+
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-content h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96"></div>
         <h1 className="mb-2 text-lg font-medium">회원가입</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <InputGroup
             placeholder="Email"
             value={email}
