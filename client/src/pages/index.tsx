8e317bcffde91793ca6fe7836493ff4d4a51daa7
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-16 15:19:02 +0900

    fix(ui) Using SWR

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,16 +5,19 @@ import Image from "next/image";
 import useSWR from "swr";
 import axios from "axios";
 import styles from "@/src/styles/Home.module.css";
-import { Sub } from "../types";
+import { Sub } from "@/src/types";
+import { useAuthState } from "@/src/context/auth";
 
 const Home: NextPage = () => {
+  const { authenticated } = useAuthState();
+
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data);
   };
 
   const address = "http://localhost:4000/api/subs/sub/topsSubs";
-
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
+  console.log("topSubs", topSubs);
 
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
@@ -28,14 +31,41 @@ const Home: NextPage = () => {
           </div>
 
           {/* 커뮤니티 리스트 */}
-          <div></div>
-          <div className="w-full py-6 text-center">
-            <Link href="/subs/create" legacyBehavior>
-              <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
-                커뮤니티 만들기
-              </a>
-            </Link>
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`} legacyBehavior>
+                  <a>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                </Link>
+                <Link href={`/r/${sub.name}`} legacyBehavior>
+                  <a className="ml-2 font-bold hover:cursor-pointer">
+                    /r/{sub.name}
+                  </a>
+                </Link>
+                <p className="ml-auto font-md">{sub.postCount}</p>
+              </div>
+            ))}
           </div>
+          {authenticated && (
+            <div className="w-full py-6 text-center">
+              <Link href="/subs/create" legacyBehavior>
+                <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
+                  커뮤니티 만들기
+                </a>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
