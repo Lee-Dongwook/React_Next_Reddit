39e04cc21222de8b8b9022422751ed4c95e21ed7
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-16 13:03:38 +0900

    feat(ui) Navbar init

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,24 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import Axios from "axios";
-import { AuthProvider } from "../context/auth";
+import { AuthProvider } from "@/src/context/auth";
+import NavBar from "@/src/components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
   Axios.defaults.withCredentials = true;
+
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      {!authRoute && <NavBar />}
+      <div className={authRoute ? "" : "pt-12"}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }
