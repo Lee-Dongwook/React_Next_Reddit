6feef30d575198d29f4dab766cbbe4a5cbadf072
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-02 18:05:03 +0900

    bug(ci) Docker Compose Database Image

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 5432,
     username: "postgres",
-    password: "1234",
+    password: "password",
     database: "postgres",
     synchronize: true,
     logging: false,
